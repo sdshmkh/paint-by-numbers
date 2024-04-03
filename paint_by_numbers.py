f25c30bd53d33b3a6fc8803ec431d49cdfe834e4
Shahaji Deshmukh <shahaji31.deshmukh@gmail.com>
2024-04-03 13:39:43 -0400

    moved from openai edit to create variation

--- paint_by_numbers.py
+++ paint_by_numbers.py
@@ -5,22 +5,24 @@ from model import create
 from model import trigger_dalle
 
 @click.command()
-@click.option('--mode', default='edit', help='create a new image using Dall-E or edit an existing image, options - e/C')
+@click.option('--mode', default='edit', help='create a new image using Dall-E or a variation of an existing image, options - v/C')
 @click.option('--image_path', default=None, help='path to the image')
 @click.option('--prompt', default=None, help='prompt to create/edit the image')
-@click.option('--palette_count', default=8, help='number of colors for your palette')
+@click.option('--palette_count', default=10, help='number of colors for your palette')
 def run(mode, image_path, prompt, palette_count):
     load_dotenv()
     click.echo("Welcome to Paint By Numbers")
     if mode == 'C' and not prompt:
         click.echo("Please enter a prompt")
         return 
-    if mode == 'e' and not image_path:
+    if mode == 'v' and not image_path:
         click.echo("Please enter a path")
         return
     
     image_path = trigger_dalle(image_path, prompt, mode)
     pbk_image_path = create(image_path, n_clusters=palette_count)
+    click.echo(pbk_image_path)
+
 
 
 if __name__ == '__main__':
