24e212074fbbb7956076b2ca5e7473e6649307f3
sdshmkh <36247925+sdshmkh@users.noreply.github.com>
2024-04-03 20:20:19 -0400

    Revert "display image (#5)" (#6)
    This reverts commit 53ed7edffb714ecc937c9cf2c2ad99e14b4f3c46.

--- paint_by_numbers.py
+++ paint_by_numbers.py
@@ -1,6 +1,5 @@
 import click
 from dotenv import load_dotenv
-import cv2
 
 from model import create
 from model import trigger_dalle
@@ -21,15 +20,8 @@ def run(mode, image_path, prompt, palette_count):
         return
     
     image_path = trigger_dalle(image_path, prompt, mode)
-    pbk_image_path, pbk_image = create(image_path, n_clusters=palette_count)
+    pbk_image_path = create(image_path, n_clusters=palette_count)
     click.echo(pbk_image_path)
-
-    cv2.imshow('Image Title', pbk_image)
-
-    # Wait for any key to close the window
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
 
 
 
